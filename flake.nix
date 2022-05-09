3589a0a739f56ae9741d0f5347a39f4e1513e8b3
hugosenari <hugosenari@gmail.com>
2022-05-09 20:40:51 -0300

    chore: re index ngxcmod

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."ngxcmod-master".dir   = "master";
-  inputs."ngxcmod-master".owner = "nim-nix-pkgs";
-  inputs."ngxcmod-master".ref   = "master";
-  inputs."ngxcmod-master".repo  = "ngxcmod";
-  inputs."ngxcmod-master".type  = "github";
-  inputs."ngxcmod-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."ngxcmod-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."ngxcmod-0_1_2".dir   = "0_1_2";
-  inputs."ngxcmod-0_1_2".owner = "nim-nix-pkgs";
-  inputs."ngxcmod-0_1_2".ref   = "master";
-  inputs."ngxcmod-0_1_2".repo  = "ngxcmod";
-  inputs."ngxcmod-0_1_2".type  = "github";
-  inputs."ngxcmod-0_1_2".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."ngxcmod-0_1_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
